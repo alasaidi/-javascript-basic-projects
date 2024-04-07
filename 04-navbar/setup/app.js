fee9941969c114eedc4b9e0b4f7345d89284ba23
alasaidi <alasaidi55gmail.com>
2024-04-07 13:07:41 +0200

    navbar project  done

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const link=document.querySelector('.links');
+const btn=document.querySelector('.nav-toggle');
+
+btn.addEventListener('click', ()=>{
+    // here what we did with the toggle is to check if the classlist contains the class show-links if yes remove it if false add it 
+   link.classList.toggle('show-links');
+})
